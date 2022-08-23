a71648fd7ad2cfe904695284f42c6a75955b2c6a
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-22 20:36:47 -0400

    Character snapshot test passing

--- src/components/Character/Character.test.jsx
+++ src/components/Character/Character.test.jsx
@@ -5,7 +5,12 @@ import Character from './Character';
 describe('Character component', () => {
   afterEach(() => cleanup());
   it('renders Character', () => {
-    const { asFragment } = render(<Character />);
+    const { asFragment } = render(
+      <Character 
+        name="something" 
+        image="https://www.something.com/image/200/200"
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
