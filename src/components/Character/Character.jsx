9ec4882bb563b48fd5b6e149217eb1db19c0fedf
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-22 20:33:19 -0400

    Character snapshot test

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Character = () => (
-
+  <h1>hello world</h1>
 );
+
+Character.propTypes = {
+  name: PropTypes.string.isRequired
+};
 
 export default Character;
