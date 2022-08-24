064ba3e06503d59ffda5d6e70523d90a07b36de9
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-23 21:57:25 -0400

    Character list map items

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useCharacters } from '../../hooks/characters';
+import Character from '../Character/Character';
 
 const CharacterList = () => {
+  const { loading, characters, error } = useCharacters();
+  const characterStuff = characters.map(character => 
+    <li key={character.id}>
+      <Character {...character}/>
+    </li>  
+  );
 
+  return (
+
+  );
 };
 
 export default CharacterList;
