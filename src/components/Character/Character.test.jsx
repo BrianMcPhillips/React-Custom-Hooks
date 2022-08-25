c699df04cbb0396d2e480721ac057ee3b62e8fe3
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-24 21:32:18 -0400

    Test passing

--- src/components/Character/Character.test.jsx
+++ src/components/Character/Character.test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Character from './Character';
 
 describe('Character component', () => {
   afterEach(() => cleanup());
   it('renders Character', () => {
     const { asFragment } = render(
-      <Character 
-        name="something" 
-        image="https://www.something.com/image/200/200"
-      />
+      <MemoryRouter>
+        <Character 
+          name="something" 
+          image="https://www.something.com/image/200/200"
+        />
+      </MemoryRouter>
     );
     expect(asFragment()).toMatchSnapshot();
   });
