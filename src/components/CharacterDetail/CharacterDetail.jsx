01728504d54085589531f61a074cd0f165b7e160
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-24 21:26:16 -0400

    Character Detail JSX

--- src/components/CharacterDetail/CharacterDetail.jsx
+++ src/components/CharacterDetail/CharacterDetail.jsx
@@ -4,12 +4,22 @@ import { useSingleCharacter } from '../../hooks/characters';
 
 const CharacterDetail = () => {
   const { id } = useParams();
-  const { loading, data, error } = useSingleCharacter(id);
+  const { loading, error, data: {
+    name,
+    image,
+    status,
+    species
+  }  } = useSingleCharacter(id);
   
   if(loading) return <h1>Loading...</h1>;
   if(error) return <h1>Something went wrong. Try again</h1>;
   return (
-
+    <div>
+      <img src={image} alt={name} />
+      <h1>{name}</h1>
+      <h4>{species}</h4>
+      <h5>{status}</h5>
+    </div>
   );
 };
 
