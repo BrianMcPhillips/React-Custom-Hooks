4e256e42d796999e9be57804a8593da18974a4d7
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-26 16:51:46 -0400

    Test behavior by Text content

--- src/components/CharacterList/CharacterList.test.jsx
+++ src/components/CharacterList/CharacterList.test.jsx
@@ -35,6 +35,7 @@ describe('CharacterList component', () => {
 
     return waitFor(() => {
       expect(characterList).not.toBeEmptyDOMElement();
+      expect(characterList).toHaveTextContent('Rick');
     });
   });
 });
