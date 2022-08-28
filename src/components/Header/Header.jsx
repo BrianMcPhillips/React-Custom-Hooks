916af5edf7db0e28781bb2246e6ba863d842f3ec
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-27 21:40:57 -0400

    Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
-
+  <header>
+    Rick and Morty
+    <div>
+      <Link to={'/'}>
+        <button>Home</button>
+      </Link>
+    </div>
+  </header>
 );
 
 export default Header;
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,10 +6,12 @@ import {
 } from 'react-router-dom';
 import CharacterDetail from '../CharacterDetail/CharacterDetail';
 import CharacterList from '../CharacterList/CharacterList';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<CharacterList />} />
         <Route path="/detail/:id" element={<CharacterDetail />} />
