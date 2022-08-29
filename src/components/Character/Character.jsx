6ba3d6378c16075249f8d755cdc166b17c818eef
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-29 10:28:57 -0400

    Character styling

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import styles from './Character.module.css';
 
 const Character = ({ name, image, id }) => (
   <Link to={`/detail/${id}`}>
-    <figure>
+    <figure className={styles.box}>
       <img src={image} alt={name} />
       <figcaption>{name}</figcaption>
     </figure>
